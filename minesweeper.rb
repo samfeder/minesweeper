e2a993f94db14bb3d96ae2ea5ee755c0261d6211
Jack Wilson and Sam Feder <jack+sam@appacademy.io>
2014-07-14 11:30:30 -0400

    create place_bomb and count_bomb methods

--- minesweeper.rb
+++ minesweeper.rb
@@ -9,7 +9,32 @@ class Minesweeper
     place_bombs(bomb_count)
   end
 
+  def place_bombs(bomb_count)
+    while count_bombs < bomb_count
+      i = rand(0...@board.size)
+      j = rand(0...@board.size)
+      @board[i][j] = :bomb
+    end
+  end
+
+  def count_bombs
+    bombs = 0
+    @board.size.times do |i|
+      @board.size.times do |j|
+        bombs += 1 if @board[i][j] == :bomb
+      end
+    end
+
+    bombs
+  end
+
+  def render
+    @board.each do |row|
+      puts "\t#{row}"
+    end
+  end
+
 end
 
 new_game = Minesweeper.new
-puts new_game
+new_game.render
