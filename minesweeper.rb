bc5c40e3a2d578e6d32c131ff796ce7784cb9a55
Jack Wilson and Sam Feder <jack+sam@appacademy.io>
2014-07-14 12:09:06 -0400

    created set_frontier method, with valid_neighbors, with bugs.

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,31 +1,64 @@
+require 'set'
+
 class Minesweeper
+  NEIGHBORS = [
+                [-1,1],
+                [-1,0],
+                [-1,-1],
+                [0,1],
+                [0,-1],
+                [1,1],
+                [1,0],
+                [1,-1]
+              ]
 
   def self.make_board(size)
-    Array.new(size){Array.new(size)}
+    Array.new(size){Array.new(size, 0)}
   end
 
   def initialize(bomb_count = 10, size = 9)
     @board = self.class.make_board(size)
+    @bomb_positions = Set.new
     place_bombs(bomb_count)
   end
 
   def place_bombs(bomb_count)
-    while count_bombs < bomb_count
+    while @bomb_positions.size < bomb_count
       i = rand(0...@board.size)
       j = rand(0...@board.size)
-      @board[i][j] = :bomb
+      p @board[i][j] = :bomb
+      @bomb_positions.add([i,j])
     end
   end
 
-  def count_bombs
-    bombs = 0
-    @board.size.times do |i|
-      @board.size.times do |j|
-        bombs += 1 if @board[i][j] == :bomb
+  def [](pos) # -> [x,y]
+    @board[pos[0], pos[1]]
+  end
+
+  def []=(pos, value) # -> [x,y]
+    @board[pos[0], pos[1]] = value
+  end
+
+  def set_frontier
+    @bomb_positions.each do |bomb|
+      valid_neighbors(bomb).each do |neighbor|
+        p neighbor
+        self[neighbor] += 1 unless self[neighbor] == :bomb
       end
     end
+  end
 
-    bombs
+  def valid_neighbors(pos)
+    positions = []
+    NEIGHBORS.each do |n|
+      p positions << combine_pos(pos, n)
+    end
+
+    positions.reject { |p| p.max >= @board.size || p.min < 0 }
+  end
+
+  def combine_pos(pos1, change)
+    [pos1[0] + change[0], pos1[1] +change[1]]
   end
 
   def render
@@ -33,8 +66,12 @@ class Minesweeper
       puts "\t#{row}"
     end
   end
-
 end
 
+
+
+
 new_game = Minesweeper.new
+new_game.render
+new_game.set_frontier
 new_game.render
